34f4cea589949271edb7f33d857772fa0d48cd3a
Vitor Marques <89469589+VitorMarques96@users.noreply.github.com>
2023-04-07 19:09:20 -0300

    Add files via upload

---
+++ BMIcalculator_v1.py
@@ -0,0 +1,8 @@
+name = ('John Smith')
+height = 1.87
+weight = 102
+bmi = weight / (height * height)
+
+print(name, 'is', height, 'meters tall,')
+print('weighs', weight, 'kilograms, and his CMI is:')
+print(bmi)
