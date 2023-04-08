62b463b025d69c010aa77b033c8d96d52753e000
Vitor Marques <89469589+VitorMarques96@users.noreply.github.com>
2023-04-08 08:10:29 -0300

    Delete BMIcalculator_v1.py

--- BMIcalculator_v1.py
+++
@@ -1,8 +0,0 @@
-name = ('John Smith')
-height = 1.87
-weight = 102
-bmi = weight / (height * height)
-
-print(name, 'is', height, 'meters tall,')
-print('weighs', weight, 'kilograms, and his CMI is:')
-print(bmi)
